24290094e6d953f9fa32cdd7fde2255349e5d480
Niki Walker <nikiwalkerdeveloper@gmail.com>
2024-03-16 22:45:03 +0000

    Project setup.

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+/// <summary>
+/// Main game flow manager for Fleet & Empire.
+/// Start here!
+/// </summary>
+public class Game : MonoBehaviour
+{
+	void Awake()
+	{
+		 
+	}
+}
