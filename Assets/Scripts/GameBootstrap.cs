94fbc1bf121008cb659dc0e9cc0801a3fe6a8b07
Niki Walker <nikiwalkerdeveloper@gmail.com>
2024-03-16 22:48:43 +0000

    Added the NetCube logic.

---
+++ Assets/Scripts/GameBootstrap.cs
@@ -0,0 +1,13 @@
+﻿using Unity.NetCode;
+
+/// <summary>
+/// Bootstrap for Fleet & Empire.
+/// </summary>
+public class GameBootstrap : ClientServerBootstrap
+{
+    public override bool Initialize(string defaultWorldName)
+    {
+        AutoConnectPort = 7979;
+        return base.Initialize(defaultWorldName);
+    }
+}
